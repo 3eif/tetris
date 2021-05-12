f0b660ccdc79109b5f0b7f4d8db3edf7064da8f4
Tetracyl <seifaziz10@gmail.com>
2021-05-11 19:17:06 -0700

    Initial commit

---
+++ src/Tetris.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Tetris extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
