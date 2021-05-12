f1e00ca23664735a984ee643bc8a2590d3d07095
Tetracyl <seifaziz10@gmail.com>
2021-05-11 19:30:14 -0700

    Create Actor, Tetris, TetrisWorld, and World classes

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,13 +1,26 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Tetris extends Application {
     public static void main(String[] args) {
-
+        launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+        stage.setTitle("Tetris");
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 500, 700);
+
+        TetrisWorld tetrisWorld = new TetrisWorld();
+        tetrisWorld.setMinHeight(scene.getHeight());
+        tetrisWorld.setMinWidth(scene.getWidth());
+        root.setCenter(tetrisWorld);
+
+        stage.setScene(scene);
+        stage.show();
+        tetrisWorld.requestFocus();
     }
 }
---
+++ src/TetrisWorld.java
@@ -0,0 +1,20 @@
+public class TetrisWorld extends World {
+//    private Score score;
+
+    public TetrisWorld() {
+//        score = new Score();
+//        score.setX(25);
+//        score.setY(50);
+//
+//        getChildren().add(score);
+    }
+
+    @Override
+    public void act() {
+
+    }
+
+//    public Score getScore() {
+//        return score;
+//    }
+}
