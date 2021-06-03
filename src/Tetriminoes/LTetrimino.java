561d9f01c64f253fba7b9bdf21c551316afadeb3
Tetracyl <seifaziz10@gmail.com>
2021-06-03 09:55:37 -0700

    Fix tetrimino shape

--- src/Tetriminoes/LTetrimino.java
+++ src/Tetriminoes/LTetrimino.java
@@ -4,8 +4,8 @@ public class LTetrimino extends Tetrimino {
     private static final int width = 3;
     private static final int height = 2;
     private static final int[][] shape = {
-            {1, 1, 1},
             {0, 0, 1},
+            {1, 1, 1},
             {0, 0, 0},
     };
 
